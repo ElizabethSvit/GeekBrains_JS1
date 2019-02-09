45c8f29594180a904c61b8e56096bd8b11da5889
svitliza <lizasvit@gmail.com>
2019-02-09 20:32:04 +0300

    task 6

--- hw2.js
+++ hw2.js
@@ -65,5 +65,12 @@ function mathOperation(arg1, arg2, operation) {
     }
 }
 
+// task 6
+function power(val, pow) {
+    if (pow === 1) return val;
+    return val * power(val, pow - 1);
+}
+
+console.log(power(2, 5));
 
 
