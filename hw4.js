b3fb5a3988decdc25c6b027d516fe761cdadab01
svitliza <lizasvit@gmail.com>
2019-02-19 00:31:36 +0300

    task 2 almost done

--- hw4.js
+++ hw4.js
@@ -19,3 +19,56 @@ function num2object(number) {
 console.log(num2object(5));
 console.log(num2object(387));
 console.log(num2object(1000));
+
+// task 2
+
+let Basket = {
+    goodList : [],
+    countTotalPrice: function() {
+        var totalPrice = 0;
+        for (var good in this.goodList) {
+            totalPrice += good.price;
+        }
+        return totalPrice;
+    },
+    countTotalNumber: function() {
+        return this.goodList.length;
+    }
+};
+
+let Good = {
+    name : 'blueberry',
+    price: 6,
+    putToBasket: function() {
+        Basket.goodList.push(this);
+    }
+};
+
+var good1 = {};
+Object.assign(good1, Good);
+good1.name = 'blueberry';
+good1.price = 6;
+
+var good2 = {};
+Object.assign(good2, Good);
+good2.name = 'strawberry';
+good2.price = 5;
+
+var good3 = {};
+Object.assign(good3, Good);
+good3.name = 'cranberry';
+good3.price = 4;
+
+var goods = [good1, good2, good3];
+
+for (var i = 0; i < goods.length; i++) {
+    if (i % 2 === 0) {
+        goods[i].putToBasket();
+    } else {
+        goods[i].putToBasket();
+        goods[i].putToBasket();
+    }
+}
+
+console.log(Basket.countTotalNumber());
+console.log(Basket.countTotalPrice());
